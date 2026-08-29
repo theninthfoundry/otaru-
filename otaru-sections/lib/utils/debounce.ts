9122c7e78ea0dc1eb322888d220961221400d314
Sreeshanth Reddy <namireddysreeshanth@gmail.com>
2026-08-29 18:58:29 +0530

    feat(utils): add debounce utility helper

---
+++ otaru-sections/lib/utils/debounce.ts
@@ -0,0 +1,10 @@
+export function debounce<Args extends unknown[]>(
+  fn: (...args: Args) => void,
+  delayMs: number
+): (...args: Args) => void {
+  let timer: ReturnType<typeof setTimeout> | undefined;
+  return (...args: Args) => {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => fn(...args), delayMs);
+  };
+}
